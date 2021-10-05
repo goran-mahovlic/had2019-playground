64df5560b4a280669577882e56b4a8886bf3b3d5
emard <vordah@gmail.com>
2021-10-05 09:40:27 +0200

    change ESP32 reboot BTN1 -> BTN2

--- projects/bootloader/rtl/esp32_passthru.v
+++ projects/bootloader/rtl/esp32_passthru.v
@@ -78,11 +78,10 @@ module esp32_passthru
   // wifi_gpio12 (must be 0 for esp32-wroom fuse unprogrammed. esp32-wrover-e works for 0 and 1)
 
   //assign wifi_en = S_prog_out[1];
-  // assign wifi_en      = R_prog_release[C_prog_release_timeout] ? 1'bz : S_prog_out[1] & R_powerup_en_time[C_powerup_en_time] & ~btn[1]; // holding BTN1 disables ESP32, releasing BTN1 reboots ESP32
-  assign wifi_en      = S_prog_out[1] & R_powerup_en_time[C_powerup_en_time] & ~btn[1] ? 1'bz : 1'b0; // holding BTN1 disables ESP32, releasing BTN1 reboots ESP32
+  assign wifi_en      = S_prog_out[1] & R_powerup_en_time[C_powerup_en_time] & ~btn[2] ? 1'bz : 1'b0; // holding BTN2 disables ESP32, releasing BTN2 reboots ESP32
   assign wifi_gpio13  = R_prog_release[C_prog_release_timeout] ? 1'bz : 1'b1;
   assign wifi_gpio12  = R_prog_release[C_prog_release_timeout] ? 1'bz : 1'b0;
-  //assign wifi_gpio12  = btn[2]; // experiment with ESP32 VRef 3.3V/1.8V
+  //assign wifi_gpio12  = btn[3]; // experiment with ESP32 VRef 3.3V/1.8V
   //assign wifi_gpio5   = R_prog_release[C_prog_release_timeout] ? 1'bz : 1'b0; // available on v3.0.x only
   assign wifi_gpio4   = R_prog_release[C_prog_release_timeout] ? 1'bz : 1'b1;
   assign wifi_gpio2   = R_prog_release[C_prog_release_timeout] ? 1'bz : S_prog_out[0];
